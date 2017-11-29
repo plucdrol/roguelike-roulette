c1288e95a379f8c4ace0be7b2b0e8d20d194f81c
Unknown <plucdrol@gmail.com>
2017-11-29 18:23:28 -0500

    html

--- roguelike-roulette.js
+++ roguelike-roulette.js
@@ -1,5 +1,15 @@
 var fastGames = ['20XX','ISAAC','NECRODANCER','DEAD CELLS','DOWNWELL','GUNGEON','FLINTHOOK','NUCLEAR THRONE','RISK OF RAIN','ROGUE LEGACY','SPELUNKY','ZIGGURAT'];
 
 var roulette = function() {
-	return arguments[Math.floor(Math.random() * (arguments.length  + 1))];
+	var random = Math.random();
+	var length = (arguments.length  + 1);
+	var findex = Math.random() * (arguments.length  + 1);	
+	var index = Math.floor(findex);
+	console.log('random:		' + random);
+	console.log('length:		' + length);
+	console.log('float index:	' + findex);
+	console.log('index:			' + index);
+	return arguments[index];
 }
+
+console.log(roulette('20XX','ISAAC','NECRODANCER','DEAD CELLS','DOWNWELL','GUNGEON','FLINTHOOK','NUCLEAR THRONE'))
